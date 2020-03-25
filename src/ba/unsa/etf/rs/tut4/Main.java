36a5aea697ff3feae96f291ffbac46793245dc3c
Emir Kurtovic <ekurtovic3@etf.unsa.ba>
2020-03-25 22:37:15 +0100

    Controler i fxml

--- src/ba/unsa/etf/rs/tut4/Main.java
+++ src/ba/unsa/etf/rs/tut4/Main.java
@@ -1,7 +1,24 @@
 package ba.unsa.etf.rs.tut4;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+
+public class Main extends Application {
+
     public static void main(String[] args) {
-        System.out.println("Sretan rad na tutorijalu 4!");
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("/fxml/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300.0D, 275.0D));
+        primaryStage.show();
     }
 }
