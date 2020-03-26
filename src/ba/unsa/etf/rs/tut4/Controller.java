d1341ebf3528e54c606620c22662cebd4be1c5ca
Emir Kurtovic <ekurtovic3@etf.unsa.ba>
2020-03-26 22:52:24 +0100

    dodajArtikle

--- src/ba/unsa/etf/rs/tut4/Controller.java
+++ src/ba/unsa/etf/rs/tut4/Controller.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rs.tut4;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import static ba.unsa.etf.rs.tut4.Artikal.izbaciDuplikate;
+
 public class Controller {
+
+
+    public TextArea unosArtikla;
+    public TextArea ispisArtikla;
+
+    public void dodajArtikle(ActionEvent actionEvent) {
+        String spisak=new String();
+        spisak=unosArtikla.getText();
+        ArrayList<Artikal> lista =new ArrayList<>();
+        for(String jedan: spisak.split("\n")){
+            Artikal pom=new Artikal(jedan);
+            lista.add(pom);
+        }
+        izbaciDuplikate(lista);
+        for(Artikal pom: lista){
+            ispisArtikla.setText(pom.toString()+"\n");
+        }
+
+
+
+
+    }
+
+
 }
