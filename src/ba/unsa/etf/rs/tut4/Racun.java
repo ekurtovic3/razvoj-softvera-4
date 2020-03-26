a72a823747f4561c1bdcf455e2e19f80e8258113
Emir Kurtovic <ekurtovic3@etf.unsa.ba>
2020-03-27 00:33:25 +0100

    Metode dodajStavku,ukupanIznos

--- src/ba/unsa/etf/rs/tut4/Racun.java
+++ src/ba/unsa/etf/rs/tut4/Racun.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rs.tut4;
 
+import java.util.ArrayList;
+
 public class Racun {
+    ArrayList<Artikal> artikli = new ArrayList<>();
+    ArrayList<Double> kolicine  = new ArrayList<>();
+
+
+    public void dodajStavku(Artikal art, double kolicina) {
+artikli.add(art);
+kolicine.add(kolicina);
+    }
+
+    public double ukupanIznos(){
+        double suma=0;
+        for(int i=0;i<artikli.size();i++){
+            suma=suma+artikli.get(i).getCijena()*kolicine.get(i);
+        }
+        return suma;
+    }
 }
